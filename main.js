02dc9d18f09e22f8f8d3b8512f8cf711396d156f
DrSlump <drslump@pollinimini.net>
2015-01-23 14:39:05 +0100

    code gardening

--- main.js
+++ main.js
@@ -27,13 +27,15 @@ ass.should.restore = function (/* name */) {
 };
 
 
-// Patch third party libraries to understand ass-ert experessions. We depend
-// on patching lodash for the library to work correctly, however the rest are
-// optional.
+// Patch third party libraries to understand about ass-ert expressions. We
+// depend on patching lodash for the library to work correctly, however the
+// rest are optional.
 patches.lodash(require('lodash'));
 
-if (global.sinon && global.sinon.match || require.resolve && require.resolve('sinon')) {
-  patches.sinon(global.sinon || require('sinon'));
+if (global.sinon && global.sinon.match) {
+  patches.sinon(global.sinon);
+} else if (require.resolve && require.resolve('sinon')) {
+  patches.sinon(require('sinon'));
 }
 
 
